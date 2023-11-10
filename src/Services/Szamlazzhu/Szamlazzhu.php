010bb45cca0e1f53e36c1bf94e070f2e5ac48c84
Támba Bence <btamba@composite.hu>
2023-11-10 19:18:13 +0100

    [update] refactor code

--- src/Services/Szamlazzhu/Szamlazzhu.php
+++ src/Services/Szamlazzhu/Szamlazzhu.php
@@ -5,10 +5,9 @@ namespace Composite\InvoiceWrapper\Services\Szamlazzhu;
 require_once __DIR__ . '/sdk_autoloader.php';
 
 use Composite\InvoiceWrapper\Interfaces\InvoiceGateway;
+use Composite\InvoiceWrapper\Traits\SzamazzhuHelper;
 use Exception;
 use SzamlaAgent\Buyer;
-use SzamlaAgent\Currency;
-use SzamlaAgent\Document\Document;
 use SzamlaAgent\Document\Invoice\Invoice;
 use SzamlaAgent\Item\InvoiceItem;
 use SzamlaAgent\Language;
@@ -16,12 +15,16 @@ use SzamlaAgent\Response\SzamlaAgentResponse;
 use SzamlaAgent\SzamlaAgent;
 use SzamlaAgent\SzamlaAgentAPI;
 use SzamlaAgent\SzamlaAgentException;
-use SzamlaAgent\TaxPayer;
 
 class Szamlazzhu implements InvoiceGateway
 {
+    use SzamazzhuHelper;
     private SzamlaAgent $client;
 
+    /**
+     * @param array $providerConfig
+     * @throws SzamlaAgentException
+     */
     public function __construct(array $providerConfig)
     {
         $this->client = SzamlaAgentAPI::create($providerConfig['api_key']);
@@ -31,6 +34,7 @@ class Szamlazzhu implements InvoiceGateway
      * @param array $invoicePayload
      * @return array
      * @throws SzamlaAgentException
+     * @throws Exception
      */
     public function issueInvoice(array $invoicePayload): array
     {
@@ -85,7 +89,6 @@ class Szamlazzhu implements InvoiceGateway
             $invoice->addItem($invoiceItem);
         }
 
-
         $response = $this->client->generateInvoice($invoice);
         if ($response->isSuccess()) {
             return $this->getInvoice($response->getData()['documentNumber']);
@@ -96,138 +99,25 @@ class Szamlazzhu implements InvoiceGateway
 
     /**
      * @param string $invoiceId
-     * @return array[]
+     * @return array
      * @throws SzamlaAgentException
+     * @throws Exception
      */
     public function getInvoice(string $invoiceId): array
     {
         $this->client->setResponseType(SzamlaAgentResponse::RESULT_AS_XML);
-
         $response = $this->client->getInvoiceData($invoiceId);
 
-        if ($response->isSuccess()) {
-            $invoiceResponse = $response->getData()['result'];
-            $items = [];
-            foreach ($invoiceResponse['tetelek']['tetel'] as $item) {
-                $items[] = [
-                    'name' => $item['nev'],
-                    'unit_net_price' => $item['nettoegysegar'],
-                    'quantity' => $item['mennyiseg'],
-                    'unit' => $item['mennyisegiegyseg'],
-                    'vat' => $item['afakulcs'], // '0%','5%','18%','27%','27%'
-                    'net_price' => $item['netto'],
-                    'vat_price' => $item['afa'],
-                    'gross_price' => $item['brutto'],
-                ];
-            }
-            return [
-                'partner' => [
-                    'id' => $invoiceResponse['vevo']['id'],
-                    'name' => $invoiceResponse['vevo']['nev'],
-                    'address' => [
-                        'country_code' => $this->getCountryCode($invoiceResponse['vevo']['cim']['orszag'] ?? ''),
-                        'post_code' => $invoiceResponse['vevo']['cim']['irsz'],
-                        'city' => $invoiceResponse['vevo']['cim']['telepules'],
-                        'address' => $invoiceResponse['vevo']['cim']['cim'],
-                    ],
-                    'taxcode' => $invoiceResponse['vevo']['adoszam'],
-                    'email' => $invoiceResponse['vevo']['email'],
-                ],
-                'invoice' => [
-                    'fulfillment_date' => $invoiceResponse['alap']['telj'],
-                    'due_date' => $invoiceResponse['alap']['fizh'],
-                    'payment_method' => $this->getPaymentMethodType($invoiceResponse['alap']['fizmod']),
-                    'language' => $invoiceResponse['alap']['nyelv'],
-                    'currency' => $invoiceResponse['alap']['devizanem'],
-//                    'paid' => true, // TODO: get from response
-                    'items' => $items,
-                    'comment' => $invoiceResponse['alap']['megjegyzes'],
-                ],
-            ];
-        } else {
-            throw new Exception($response->isFailed());
+        if (!$response->isSuccess()) {
+            throw new Exception('Invoice data retrieval failed.');
         }
+
+        return $this->formatInvoiceResponse($response->getData()['result']);
     }
 
     public function downloadInvoice(int $invoiceId): array
     {
-        return ["Szamazz.hu"];
-    }
-
-    private function getPaymentMethod($method): string
-    {
-        return match ($method) {
-            'bankcard' => Document::PAYMENT_METHOD_BANKCARD,
-            'cash' => Document::PAYMENT_METHOD_CASH,
-            'wire_transfer' => Document::PAYMENT_METHOD_TRANSFER,
-            'cash_on_delivery' => Document::PAYMENT_METHOD_CASH_ON_DELIVERY,
-            'paypal' => Document::PAYMENT_METHOD_PAYPAL,
-            'szep_card' => Document::PAYMENT_METHOD_SZEP_CARD,
-            default => Document::PAYMENT_METHOD_CASH,
-        };
-    }
-
-    private function getPaymentMethodType($method): string
-    {
-        return match ($method) {
-            Document::PAYMENT_METHOD_BANKCARD => 'bankcard',
-            Document::PAYMENT_METHOD_CASH => 'cash',
-            Document::PAYMENT_METHOD_TRANSFER => 'wire_transfer',
-            Document::PAYMENT_METHOD_CASH_ON_DELIVERY => 'cash_on_delivery',
-            Document::PAYMENT_METHOD_PAYPAL => 'paypal',
-            Document::PAYMENT_METHOD_SZEP_CARD => 'szep_card',
-            default => 'cash',
-        };
-    }
-
-    private function getCurrency($currency): string
-    {
-        return match ($currency) {
-            'HUF' => Currency::CURRENCY_HUF,
-            'EUR' => Currency::CURRENCY_EUR,
-            default => Currency::CURRENCY_HUF,
-        };
-    }
-
-    private function getLanguage($language): ?string
-    {
-        return match ($language) {
-            'hu' => Language::LANGUAGE_HU,
-            'en' => Language::LANGUAGE_EN,
-            default => null,
-        };
-    }
-
-    private function getTaxPayer($taxType): int
-    {
-        return match ($taxType) {
-            'HAS_TAX_NUMBER' => TaxPayer::TAXPAYER_HAS_TAXNUMBER,
-            default => TaxPayer::TAXPAYER_NO_TAXNUMBER,
-        };
-    }
-
-    private function getVat($vatRate): string
-    {
-        return match ($vatRate) {
-            '5', '18', '27', '0' => $vatRate,
-            default => '27',
-        };
-    }
-
-    private function getCountryCode($countryName): string
-    {
-        return match ($countryName) {
-            'Magyarország' => 'HU', // TODO: get from config, add more countries
-            default => $countryName,
-        };
-    }
-
-    private function getCountryName($countryCode): string
-    {
-        return match ($countryCode) {
-            'HU' => 'Magyarország', // TODO: get from config, add more countries
-            default => $countryCode,
-        };
+        return ["Szamlazz.hu"];
     }
 
 }
---
+++ src/Traits/SzamazzhuHelper.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Composite\InvoiceWrapper\Traits;
+
+use SzamlaAgent\Currency;
+use SzamlaAgent\Document\Document;
+use SzamlaAgent\Language;
+use SzamlaAgent\TaxPayer;
+
+trait SzamazzhuHelper
+{
+    /**
+     * @param array $invoiceResponse
+     * @return array
+     */
+    private function formatInvoiceResponse(array $invoiceResponse): array
+    {
+        $items = array_map(function ($item) {
+            return $this->formatItem($item);
+        }, $invoiceResponse['tetelek']['tetel']);
+
+        return [
+            'partner' => $this->formatPartner($invoiceResponse['vevo']),
+            'invoice' => [
+                'fulfillment_date' => $invoiceResponse['alap']['telj'],
+                'due_date' => $invoiceResponse['alap']['fizh'],
+                'payment_method' => $this->getPaymentMethodType($invoiceResponse['alap']['fizmod']),
+                'language' => $invoiceResponse['alap']['nyelv'],
+                'currency' => $invoiceResponse['alap']['devizanem'],
+                // 'paid' => $this->getPaymentStatus($invoiceResponse), // Assuming a method to determine this
+                'items' => $items,
+                'comment' => $invoiceResponse['alap']['megjegyzes'],
+            ],
+        ];
+    }
+
+    /**
+     * @param array $partner
+     * @return array
+     */
+    private function formatPartner(array $partner): array
+    {
+        return [
+            'id' => $partner['id'],
+            'name' => $partner['nev'],
+            'address' => $this->formatAddress($partner['cim']),
+            'taxcode' => $partner['adoszam'],
+            'email' => $partner['email'],
+        ];
+    }
+
+    /**
+     * @param array $address
+     * @return array
+     */
+    private function formatAddress(array $address): array
+    {
+        return [
+            'country_code' => $this->getCountryCode($address['orszag'] ?? ''),
+            'post_code' => $address['irsz'],
+            'city' => $address['telepules'],
+            'address' => $address['cim'],
+        ];
+    }
+
+    /**
+     * @param array $item
+     * @return array
+     */
+    private function formatItem(array $item): array
+    {
+        return [
+            'name' => $item['nev'],
+            'unit_net_price' => $item['nettoegysegar'],
+            'quantity' => $item['mennyiseg'],
+            'unit' => $item['mennyisegiegyseg'],
+            'vat' => $item['afakulcs'],
+            'net_price' => $item['netto'],
+            'vat_price' => $item['afa'],
+            'gross_price' => $item['brutto'],
+        ];
+    }
+
+    /**
+     * @param $method
+     * @return string
+     */
+    private function getPaymentMethod($method): string
+    {
+        return match ($method) {
+            'bankcard' => Document::PAYMENT_METHOD_BANKCARD,
+            'cash' => Document::PAYMENT_METHOD_CASH,
+            'wire_transfer' => Document::PAYMENT_METHOD_TRANSFER,
+            'cash_on_delivery' => Document::PAYMENT_METHOD_CASH_ON_DELIVERY,
+            'paypal' => Document::PAYMENT_METHOD_PAYPAL,
+            'szep_card' => Document::PAYMENT_METHOD_SZEP_CARD,
+            default => Document::PAYMENT_METHOD_CASH,
+        };
+    }
+
+    /**
+     * @param $method
+     * @return string
+     */
+    private function getPaymentMethodType($method): string
+    {
+        return match ($method) {
+            Document::PAYMENT_METHOD_BANKCARD => 'bankcard',
+            Document::PAYMENT_METHOD_CASH => 'cash',
+            Document::PAYMENT_METHOD_TRANSFER => 'wire_transfer',
+            Document::PAYMENT_METHOD_CASH_ON_DELIVERY => 'cash_on_delivery',
+            Document::PAYMENT_METHOD_PAYPAL => 'paypal',
+            Document::PAYMENT_METHOD_SZEP_CARD => 'szep_card',
+            default => 'cash',
+        };
+    }
+
+    /**
+     * @param $currency
+     * @return string
+     */
+    private function getCurrency($currency): string
+    {
+        return match ($currency) {
+            'HUF' => Currency::CURRENCY_HUF,
+            'EUR' => Currency::CURRENCY_EUR,
+            default => Currency::CURRENCY_HUF,
+        };
+    }
+
+    /**
+     * @param $language
+     * @return string|null
+     */
+    private function getLanguage($language): ?string
+    {
+        return match ($language) {
+            'hu' => Language::LANGUAGE_HU,
+            'en' => Language::LANGUAGE_EN,
+            default => null,
+        };
+    }
+
+    /**
+     * @param $taxType
+     * @return int
+     */
+    private function getTaxPayer($taxType): int
+    {
+        return match ($taxType) {
+            'HAS_TAX_NUMBER' => TaxPayer::TAXPAYER_HAS_TAXNUMBER,
+            default => TaxPayer::TAXPAYER_NO_TAXNUMBER,
+        };
+    }
+
+    /**
+     * @param $vatRate
+     * @return string
+     */
+    private function getVat($vatRate): string
+    {
+        return match ($vatRate) {
+            '5', '18', '27', '0' => $vatRate,
+            default => '27',
+        };
+    }
+
+    /**
+     * @param $countryName
+     * @return string
+     */
+    private function getCountryCode($countryName): string
+    {
+        return match ($countryName) {
+            'Magyarország' => 'HU', // TODO: get from config, add more countries
+            default => $countryName,
+        };
+    }
+
+    /**
+     * @param $countryCode
+     * @return string
+     */
+    private function getCountryName($countryCode): string
+    {
+        return match ($countryCode) {
+            'HU' => 'Magyarország', // TODO: get from config, add more countries
+            default => $countryCode,
+        };
+    }
+}
